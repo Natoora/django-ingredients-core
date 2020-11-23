98be8d4e7e6d50bd911b16b81ba4f15dac5f2777
Carlos Eduardo <carlos@natoora.com>
2020-11-23 19:12:34 +0000

    removing dependencies

--- ingredients_core/migrations/0001_initial.py
+++ ingredients_core/migrations/0001_initial.py
@@ -8,10 +8,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("contenttypes", "0001_initial"),
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
